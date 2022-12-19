2b4c8c684960cfc7465b6897c1e24dcdff64713c
Karim <kizpucci@gmail.com>
2022-12-19 15:54:23 +0100

    second query group by

--- query-join.sql
+++ query-join.sql
@@ -1,5 +1,10 @@
-1. Contare quanti iscritti ci sono stati ogni anno
+-- 1. Contare quanti iscritti ci sono stati ogni anno
 
 SELECT COUNT(*) as iscrittiTotali, YEAR(enrolment_date) AS anno
 FROM students
 GROUP BY anno
+
+-- 2. Contare gli insegnanti che hanno l'ufficio nello stesso edificio
+SELECT COUNT(*) as num_teachers, office_address 
+FROM teachers 
+GROUP BY office_address
